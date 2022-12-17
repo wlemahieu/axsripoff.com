fdffd1eae90f7e425d7975c14ea0923b376a5ef7
Wesley LeMahieu <lateralis3k@gmail.com>
2022-12-17 12:47:22 -0800

    Register Firebase app, finish config

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -7,21 +7,22 @@ import { render } from 'solid-js/web';
 import App from '@components/App';
 import { FirebaseProvider } from 'solid-firebase';
 import { initializeApp as initFirebase } from 'firebase/app';
+import { getAnalytics, logEvent } from 'firebase/analytics';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  apiKey: 'AIzaSyD7M3lTsghsjLnNJw4_dy39qjDbvTjIoZg',
   authDomain: 'axsripoff-com.firebaseapp.com',
-  // The value of `databaseURL` depends on the location of the database
-  // databaseURL: "https://DATABASE_NAME.firebaseio.com",
   projectId: 'axsripoff-com',
-  // storageBucket: "PROJECT_ID.appspot.com",
-  // messagingSenderId: "SENDER_ID",
-  appId: 'axsripoff-com',
-  // For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
-  // measurementId: "G-MEASUREMENT_ID",
+  storageBucket: 'axsripoff-com.appspot.com',
+  messagingSenderId: '107152835380',
+  appId: '1:107152835380:web:f96552196fc70830321b61',
+  measurementId: 'G-M4CR2F73FE',
 };
 
-initFirebase(firebaseConfig);
+const app = initFirebase(firebaseConfig);
+const analytics = getAnalytics(app);
+
+logEvent(analytics, 'app started');
 
 render(
   () => (
