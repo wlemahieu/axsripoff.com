b3c5362439b06c831cd19845e563856bbd174fe7
Wesley LeMahieu <wesleylemahieu@gmail.com>
2022-12-21 13:46:16 -0800

    Add logo to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,9 @@
 import { FC } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from '@components/Header.module.css';
+import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
+import logo from '@assets/android-chrome-512x512.png';
 import Navigation from '@components/Navigation';
 import Link from '@mui/material/Link';
 
@@ -9,6 +11,7 @@ const Header: FC = () => {
   const navigate = useNavigate();
   return (
     <header className={styles.root}>
+      <Avatar className={styles.logo} src={logo} onClick={() => navigate('/')} />
       <Link onClick={() => navigate('/')} color="inherit" className={styles.link}>
         <Typography variant="h1" gutterBottom>
           axsripoff.com
