c18428f37a39c8b823ff6ea49c2e8a4c2c2bad54
Wesley LeMahieu <wesleylemahieu@gmail.com>
2023-01-04 06:52:39 -0800

    handle undefined categories/id

--- src/views/Tweets.tsx
+++ src/views/Tweets.tsx
@@ -32,11 +32,10 @@ const TweetComponent: FC<PropsI> = ({ id, categories }: PropsI) => {
     if (!loaded) setLoaded(true);
   }, [isIntersecting]);
 
-  console.log({ id, categories });
   return (
     <Box ref={containerRef} sx={{ mt: 2, mb: 2, minWidth: '550px', minHeight: '200px' }}>
       <Box>
-        {categories.map((category: string) => (
+        {categories?.map((category: string) => (
           <Chip label={startCase(category)} sx={{ ml: 1 }} color="error" />
         ))}
       </Box>
